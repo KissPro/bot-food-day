c9656628ca24f9d13ffa6a6081b2cf310250a5b1
KissPro <41913032+KissPro@users.noreply.github.com>
2019-07-19 09:18:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -76,3 +76,87 @@ function sendMessage(senderId, message) {
   });
 }
 
+//add new
+function imageTemplate(type, sender_id){
+    return {
+        "attachment":{
+            "type":"image",
+            "payload":{
+                "url": getImage(type, sender_id),
+                "is_reusable":true
+            }
+        }
+    }
+}
+
+function getImage(type, sender_id){
+    // create user if doesn't exist
+    if(users[sender_id] === undefined){
+        users = Object.assign({
+            [sender_id] : {
+                'cats_count' : 0,
+                'dogs_count' : 0
+            }
+        }, users);
+    }
+
+    let count = images[type].length, // total available images by type
+        user = users[sender_id], // // user requesting image
+        user_type_count = user[type+'_count'];
+
+
+    // update user before returning image
+    let updated_user = {
+        [sender_id] : Object.assign(user, {
+            [type+'_count'] : count === user_type_count + 1 ? 0 : user_type_count + 1
+        })
+    };
+    // update users
+    users = Object.assign(users, updated_user);
+
+    console.log(users);
+    return images[type][user_type_count];
+}
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = askTemplate();
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'CAT_PICS') {
+        response = imageTemplate('cats', sender_psid);
+        callSendAPI(sender_psid, response, function(){
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if (payload === 'DOG_PICS') {
+        response = imageTemplate('dogs', sender_psid);
+        callSendAPI(sender_psid, response, function(){
+            callSendAPI(sender_psid, askTemplate('Show me more'));
+        });
+    } else if(payload === 'GET_STARTED'){
+        response = askTemplate('Are you a Cat or Dog Person?');
+        callSendAPI(sender_psid, response);
+    }
+    // Send the message to acknowledge the postback
+}
+
+
+
+
+
